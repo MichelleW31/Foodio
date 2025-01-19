23decb677259bb5a5a7180c32b4716096b45ac2a
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2025-01-18 23:22:15 -0700

    add recipes api call

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,5 +1,6 @@
 // BASE MODULES
-// import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -11,6 +12,26 @@ import './App.css';
 import Header from '../components/Header/Header';
 
 function App() {
+  const getRecipes = async () => {
+    await axios
+      .get(
+        `${import.meta.env.VITE_BASE_URL}/random?number=50&apiKey=${
+          import.meta.env.VITE_API_KEY
+        }`
+      )
+      .then((response) => {
+        console.log('response', response.data.recipes);
+      })
+      .catch((error) => {
+        console.log('error', error);
+        throw new Error(`Get recipes error, ${error}`);
+      });
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
